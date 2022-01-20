45e47239907710830a6b00eed9cc36d3e678bc02
sean-novobi <83441847+sean-novobi@users.noreply.github.com>
2022-01-20 00:21:46 -0800

    Update v5

--- trismart_smartboard_integration/models/smartboard_connector.py
+++ trismart_smartboard_integration/models/smartboard_connector.py
@@ -4,55 +4,66 @@ class SmartBoard(models.Model):
     _name = "smartboard.connector"
     _description = "SmartBoard Connector"
 
+    def param_check(self, sb_lead_id, x_api_key):
+        if not sb_lead_id:
+            return {"status": 400, "error": "Smartboard ID is null", "odoo_customer_id":False}
+        elif not x_api_key:
+            return {"status": 400, "error": "X API Key is null", "odoo_customer_id": False}
+        elif type(sb_lead_id) != int:
+            return {"status": 400, "error": "Smartboard ID is not an Integer", "odoo_customer_id": False}
+        elif type(x_api_key) != str:
+            return {"status": 400, "error": "X API Key is not an Integer", "odoo_customer_id": False}
+        else:
+            return 'Pass'
+
     @api.model
     def create_project(self, sb_lead_id, x_api_key, project_template_id=None):
         try:
-            if sb_lead_id and x_api_key:
-                if type(sb_lead_id) == int and type(x_api_key) == str:
-                    partner_exist = self.env['res.partner'].search([('sb_lead_id', '=', sb_lead_id)], limit=1)
-                    lead_exist = self.env['crm.lead'].search([('sb_lead_id', '=', sb_lead_id)], limit=1)
-                    project_exist = self.env['project.project'].search([('sb_lead_id', '=', sb_lead_id)], limit=1)
+            # Check input parameters:
+            x = self.param_check(sb_lead_id, x_api_key)
+            if x != 'Pass':
+                return x
 
-                    if not partner_exist:
-                        # Create new partner
-                        partner_new = self.env['res.partner'].create({'name': 'SBLead-' + str(sb_lead_id), 'sb_lead_id': sb_lead_id, 'x_api_key': x_api_key, "sync_status" : '1'})
-                    else:
-                        partner_exist.write({'x_api_key': x_api_key})
-                    partner_record = partner_exist if partner_exist else partner_new
+            partner_exist = self.env['res.partner'].search([('sb_lead_id', '=', sb_lead_id)], limit=1)
+            lead_exist = self.env['crm.lead'].search([('sb_lead_id', '=', sb_lead_id)], limit=1)
+            project_exist = self.env['project.project'].search([('sb_lead_id', '=', sb_lead_id)], limit=1)
 
-                    if not lead_exist:
-                        # Create new lead
-                        lead_new = self.env['crm.lead'].create({'name': 'SBLead-' + str(sb_lead_id), 'sb_lead_id': sb_lead_id})
-                        # Convert lead to opportunity
-                        vals = lead_new._convert_opportunity_data(partner_record, team_id=False)
-                        lead_new.write(vals)
-
-                    if not project_exist:
-                        project_env = self.env['project.project']
-                        # Create new project
-                        if project_template_id: # Template ID is provided
-                            if type(project_template_id) == int:
-                                template = project_env.search([('id', '=', project_template_id), ('name', 'like', ' (TEMPLATE)')])
-                                if template: # Template exists
-                                    project = template.create_project_from_template(sb_lead_id)
-                                    project_new = project_env.search([('id', '=', project['res_id'])], limit=1)
-                                else: # Template does not exist, treat as no template ID
-                                    project_new = project_env.create({"name": 'SBLead-' + str(sb_lead_id), "sb_lead_id": sb_lead_id})
-                            else: # Template ID is not an Integer, treat as no template ID
-                                project_new = project_env.create({"name": 'SBLead-' + str(sb_lead_id), "sb_lead_id": sb_lead_id})
-                        else: # Template ID is not provided
-                            project_new = project_env.create({"name": 'SBLead-' + str(sb_lead_id), "sb_lead_id": sb_lead_id})
-                        # Add partner_id
-                        project_new.partner_id = partner_record.id
-
-                    if partner_exist:
-                        return {"status": 200, "error": "", "odoo_customer_id": False}
-                    else:
-                        return {"status": 200, "error": "", "odoo_customer_id": partner_new.id}
-                else:
-                    return {"status": 400, "error": "Unsupported type for either smartboard ID or X API Key", "odoo_customer_id": False}
+            if not partner_exist:
+                # Create new partner
+                partner_new = self.env['res.partner'].create({'name': 'SBLead-' + str(sb_lead_id), 'sb_lead_id': sb_lead_id, 'x_api_key': x_api_key, "sync_status" : '1'})
             else:
-                return {"status": 400, "error": "Either Smartboard ID or X API Key is null", "odoo_customer_id":False}
+                partner_exist.write({'x_api_key': x_api_key})
+            partner_record = partner_exist if partner_exist else partner_new
+
+            if not lead_exist:
+                # Create new lead
+                lead_new = self.env['crm.lead'].create({'name': 'SBLead-' + str(sb_lead_id), 'sb_lead_id': sb_lead_id})
+                # Convert lead to opportunity
+                vals = lead_new._convert_opportunity_data(partner_record, team_id=False)
+                lead_new.write(vals)
+
+            if not project_exist:
+                project_env = self.env['project.project']
+                # Create new project
+                if project_template_id: # Template ID is provided
+                    if type(project_template_id) == int:
+                        template = project_env.search([('id', '=', project_template_id), ('name', 'like', ' (TEMPLATE)')])
+                        if template: # Template exists
+                            project = template.create_project_from_template(sb_lead_id)
+                            project_new = project_env.search([('id', '=', project['res_id'])], limit=1)
+                        else: # Template does not exist, treat as no template ID
+                            project_new = project_env.create({"name": 'SBLead-' + str(sb_lead_id), "sb_lead_id": sb_lead_id})
+                    else: # Template ID is not an Integer, treat as no template ID
+                        project_new = project_env.create({"name": 'SBLead-' + str(sb_lead_id), "sb_lead_id": sb_lead_id})
+                else: # Template ID is not provided
+                    project_new = project_env.create({"name": 'SBLead-' + str(sb_lead_id), "sb_lead_id": sb_lead_id})
+                # Add partner_id
+                project_new.partner_id = partner_record.id
+
+            if partner_exist:
+                return {"status": 200, "error": "", "odoo_customer_id": False}
+            else:
+                return {"status": 200, "error": "", "odoo_customer_id": partner_new.id}
         except Exception as e:
             return {"status": 500,  "error": "Exception occurred. Detail: %s" % e, "odoo_customer_id": False}
 
--- trismart_smartboard_integration/test_client/rpc_test.py
+++ trismart_smartboard_integration/test_client/rpc_test.py
@@ -1,18 +1,24 @@
 # from xmlrpc import client
 import odoorpc
 
+# db_name = 'demo1'
+# username = 'admin'
+# api_key = 'admin'
+# server_url = ['localhost', 8069]
+
 db_name = 'trismart-staging-15-0-3640780'
 username = 'admin'
 api_key = '0c5608e57939826227c64338c981070573de0700'
-server_url = ['https://trismart-staging-15-0-3640780.dev.odoo.com']
+server_url = ['https://trismart-staging.odoo.com']
 
 sb_lead_id = 14
 project_template_id = 2
+x_api_key = 'bWFnZ2llOnN1bW1lcnM'
 
 odoo = odoorpc.ODOO(server_url[0], port=server_url[1])
 odoo.login(db_name, username, api_key)
 
-user_data = odoo.execute('smartboard.connector', 'create_project', sb_lead_id, project_template_id)
+user_data = odoo.execute('smartboard.connector', 'create_project', sb_lead_id, x_api_key, project_template_id)
 print(user_data)
 
 
