f4305f2dcad8fe62b215e9a88200c7131c1d1005
sean-novobi <83441847+sean-novobi@users.noreply.github.com>
2022-01-21 00:39:27 -0800

    Update ticket TRIS-57 V6

--- trismart_smartboard_integration/models/smartboard_connector.py
+++ trismart_smartboard_integration/models/smartboard_connector.py
@@ -4,25 +4,27 @@ class SmartBoard(models.Model):
     _name = "smartboard.connector"
     _description = "SmartBoard Connector"
 
-    def param_check(self, sb_lead_id, x_api_key):
-        if not sb_lead_id:
-            return {"status": 400, "error": "Smartboard ID is null", "odoo_customer_id":False}
-        elif not x_api_key:
-            return {"status": 400, "error": "X API Key is null", "odoo_customer_id": False}
-        elif type(sb_lead_id) != int:
-            return {"status": 400, "error": "Smartboard ID is not an Integer", "odoo_customer_id": False}
-        elif type(x_api_key) != str:
-            return {"status": 400, "error": "X API Key is not an Integer", "odoo_customer_id": False}
-        else:
-            return 'Pass'
+    def param_check(self, input_dict):
+        res = []
+        if not input_dict['sb_lead_id']:
+            res = {"status": 400, "error": "Smartboard ID is null", "odoo_customer_id":False}
+        elif not input_dict['x_api_key']:
+            res = {"status": 400, "error": "X API Key is null", "odoo_customer_id": False}
+        elif type(input_dict['sb_lead_id']) != int:
+            res = {"status": 400, "error": "Smartboard ID is not an Integer", "odoo_customer_id": False}
+        elif type(input_dict['x_api_key']) != str:
+            res = {"status": 400, "error": "X API Key is not an Integer", "odoo_customer_id": False}
+        elif input_dict['project_template_id'] and type(input_dict['project_template_id']) == int:
+            res = "is_temp"
+        return res
 
     @api.model
     def create_project(self, sb_lead_id, x_api_key, project_template_id=None):
         try:
+            input_dict = {'sb_lead_id' : sb_lead_id, 'x_api_key' : x_api_key, 'project_template_id' : project_template_id}
             # Check input parameters:
-            x = self.param_check(sb_lead_id, x_api_key)
-            if x != 'Pass':
-                return x
+            if self.param_check(input_dict) and self.param_check(input_dict) != "is_temp":
+                return self.param_check(input_dict)
 
             partner_exist = self.env['res.partner'].search([('sb_lead_id', '=', sb_lead_id)], limit=1)
             lead_exist = self.env['crm.lead'].search([('sb_lead_id', '=', sb_lead_id)], limit=1)
@@ -45,23 +47,20 @@ class SmartBoard(models.Model):
             if not project_exist:
                 project_env = self.env['project.project']
                 # Create new project
-                if project_template_id: # Template ID is provided
-                    if type(project_template_id) == int:
-                        template = project_env.search([('id', '=', project_template_id), ('name', 'like', ' (TEMPLATE)')])
-                        if template: # Template exists
-                            project = template.create_project_from_template(sb_lead_id)
-                            project_new = project_env.search([('id', '=', project['res_id'])], limit=1)
-                        else: # Template does not exist, treat as no template ID
-                            project_new = project_env.create({"name": 'SBLead-' + str(sb_lead_id), "sb_lead_id": sb_lead_id})
-                    else: # Template ID is not an Integer, treat as no template ID
+                if self.param_check(input_dict) == 'is_temp':
+                    template = project_env.search([('id', '=', project_template_id), ('name', 'like', ' (TEMPLATE)')])
+                    if template:  # Template exists
+                        project = template.create_project_from_template(sb_lead_id)
+                        project_new = project_env.search([('id', '=', project['res_id'])], limit=1)
+                    else:
                         project_new = project_env.create({"name": 'SBLead-' + str(sb_lead_id), "sb_lead_id": sb_lead_id})
-                else: # Template ID is not provided
+                else:
                     project_new = project_env.create({"name": 'SBLead-' + str(sb_lead_id), "sb_lead_id": sb_lead_id})
                 # Add partner_id
                 project_new.partner_id = partner_record.id
 
             if partner_exist:
-                return {"status": 200, "error": "", "odoo_customer_id": False}
+                return {"status": 200, "error": "", "odoo_customer_id": partner_exist.id}
             else:
                 return {"status": 200, "error": "", "odoo_customer_id": partner_new.id}
         except Exception as e:
