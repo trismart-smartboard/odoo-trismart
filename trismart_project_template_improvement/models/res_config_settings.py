5a878849eccc348e95a5df8953b490a542229f65
sean-novobi <83441847+sean-novobi@users.noreply.github.com>
2022-02-10 07:46:41 -0800

    Update TRIS-91
    Change the method to set default project template

--- trismart_project_template_improvement/models/res_config_settings.py
+++ trismart_project_template_improvement/models/res_config_settings.py
@@ -6,23 +6,9 @@ class ResConfigSettings(models.TransientModel):
 
     user_default_project_template = fields.Many2one(
         'project.project',
-        default_model = 'project.project',
         string = 'Default Project Template',
         domain="[('is_template', '=', True)]",
-        config_parameter='trismart_project_template_improvement.user_default_project_template',
+        related="company_id.user_default_project_template",
+        readonly = False,
         help='Project Template to use when create new project')
-
-    @api.model
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        res.update(
-            user_default_project_template = int(self.env['ir.config_parameter'].sudo().get_param('trismart_project_template_improvement.user_default_project_template')),
-        )
-        return res
-
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        param = self.env['ir.config_parameter'].sudo()
-        value = self.user_default_project_template and self.user_default_project_template.id or False
-        param.set_param('trismart_project_template_improvement.user_default_project_template', value)
 
