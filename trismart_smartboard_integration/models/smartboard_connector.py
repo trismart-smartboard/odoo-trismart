4217151f8b62ad4402850651f572582fd5e724b2
sean-novobi <83441847+sean-novobi@users.noreply.github.com>
2022-02-14 23:42:15 -0800

    Improve Code TRIS-57 v3

--- trismart_smartboard_integration/models/smartboard_connector.py
+++ trismart_smartboard_integration/models/smartboard_connector.py
@@ -25,7 +25,7 @@ class SmartBoard(models.Model):
 
     def create_non_template_project (self, sb_lead_id):
         project_env = self.env['project.project']
-        default_template_id = self.env.user.company_id.user_default_project_template.id or False
+        default_template_id = self.company_id.user_default_project_template.id or False
         if default_template_id:
             default_template = project_env.search([('id', '=', default_template_id)])
             project = default_template.create_project_from_template(sb_lead_id)
@@ -64,13 +64,10 @@ class SmartBoard(models.Model):
             if not project_exist:
                 project_env = self.env['project.project']
                 # Create new project
-                if project_template_id:
-                    template = project_env.search([('id', '=', project_template_id), ('is_template', '=', True)])
-                    if template:  # Template exists
-                        project = template.create_project_from_template(sb_lead_id)
-                        project_new = project_env.search([('id', '=', project['res_id'])], limit=1)
-                    else:
-                        project_new = self.create_non_template_project(sb_lead_id)
+                template = project_env.search([('id', '=', project_template_id), ('is_template', '=', True)]) if project_template_id else False
+                if template:  # Template exists
+                    project = template.create_project_from_template(sb_lead_id)
+                    project_new = project_env.search([('id', '=', project['res_id'])], limit=1)
                 else:
                     project_new = self.create_non_template_project(sb_lead_id)
                 # Add partner_id
